24acd05636269d65f09e9d813b4db46a70b43ec6
Nicola Piga <nicolapiga@gmail.com>
2019-12-19 15:52:30 +0100

    Implement class RobotsIO::Utils::Parameters

---
+++ src/RobotsIO/include/RobotsIO/Utils/Parameters.h
@@ -0,0 +1,179 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_PARAMETERS_H
+#define ROBOTSIO_PARAMETERS_H
+
+#include <RobotsIO/Utils/ParametersExtractor.h>
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace RobotsIO {
+    namespace Utils {
+        class Parameters;
+    }
+}
+
+#define robots_io_field_storage(type) \
+    std::unordered_map<std::string, type > type##_data_; \
+
+#define robots_io_std_field_storage(type) \
+    std::unordered_map<std::string, std::type > type##_data_; \
+
+#define robots_io_field_getter(type) \
+    type get_##type(const std::string& name) const; \
+
+#define robots_io_std_field_getter(type) \
+    std::type get_##type(const std::string& name) const; \
+
+#define robots_io_field_setter(type) \
+    void set_##type(const std::string& name, const type& value); \
+
+#define robots_io_std_field_setter(type) \
+    void set_##type(const std::string& name, const std::type& value); \
+
+#define robots_io_declare_field(class_name, type, name) \
+    type name() const; \
+    \
+    void name(const type& value); \
+    \
+    class Field_##name : public Field \
+    { \
+        public: \
+        void extract_field(const class_name &parameters, RobotsIO::Utils::ParametersExtractor& extractor) const override; \
+    }; \
+    \
+    friend class Field_##name;
+
+#define robots_io_declare_std_field(class_name, type, name) \
+    std::type name() const; \
+    \
+    void name(const std::type& value); \
+    \
+    class Field_##name : public Field \
+    { \
+        public: \
+        void extract_field(const class_name &parameters, RobotsIO::Utils::ParametersExtractor& extractor) const override; \
+    }; \
+    \
+    friend class Field_##name;
+
+#define robots_io_accessor(class_name) \
+    class Field \
+    { \
+    public: \
+        virtual void extract_field(const class_name& parameters, RobotsIO::Utils::ParametersExtractor& extractor) const = 0; \
+    }; \
+    \
+    std::unordered_map<std::string, Field*> fields_; \
+    \
+    void extract_field(const std::string& key, RobotsIO::Utils::ParametersExtractor& extractor) const override; \
+    \
+    std::vector<std::string> keys() const override;
+
+#define robots_io_declare_field_impl(class_name, type, name) \
+    type class_name::name() const \
+    { \
+        return get_##type(#name); \
+    } \
+    \
+    void class_name::name(const type& value) \
+    { \
+        if (fields_.find(#name) == fields_.end()) \
+            fields_[#name] = new Field_##name(); \
+        set_##type(#name, value); \
+    } \
+    \
+    void class_name::Field_##name::extract_field(const class_name& parameters, RobotsIO::Utils::ParametersExtractor& extractor) const \
+    { \
+        extractor.extract_field(#name, parameters.name()); \
+    } \
+
+#define robots_io_declare_std_field_impl(class_name, type, name) \
+    std::type class_name::name() const \
+    { \
+        return get_##type(#name); \
+    } \
+    \
+    void class_name::name(const std::type& value) \
+    { \
+        if (fields_.find(#name) == fields_.end()) \
+            fields_[#name] = new Field_##name(); \
+        set_##type(#name, value); \
+    } \
+    \
+    void class_name::Field_##name::extract_field(const class_name& parameters, RobotsIO::Utils::ParametersExtractor& extractor) const \
+    { \
+        extractor.extract_field(#name, parameters.name()); \
+    } \
+
+#define robots_io_accessor_impl(class_name) \
+    void class_name::extract_field(const std::string& key, RobotsIO::Utils::ParametersExtractor& extractor) const \
+    { \
+        fields_.at(key)->extract_field(*this, extractor); \
+    } \
+    \
+    std::vector<std::string> class_name::keys() const\
+    { \
+        std::vector<std::string> keys; \
+        for (const auto& field : fields_) \
+            keys.push_back(field.first); \
+        return keys; \
+    } \
+
+class RobotsIO::Utils::Parameters
+{
+public:
+    /**
+     * Field extractor from name
+     */
+    virtual void extract_field(const std::string& key, RobotsIO::Utils::ParametersExtractor& extractor) const = 0;
+
+    /**
+     * Field keys accessor
+     */
+    virtual std::vector<std::string> keys() const = 0;
+
+protected:
+    /**
+     * Getters
+     */
+    robots_io_std_field_getter(string);
+
+    robots_io_field_getter(double);
+
+    robots_io_field_getter(int);
+
+    robots_io_field_getter(bool);
+
+    /**
+     * Setters
+     */
+    robots_io_std_field_setter(string);
+
+    robots_io_field_setter(double);
+
+    robots_io_field_setter(int);
+
+    robots_io_field_setter(bool);
+
+private:
+    /**
+     * Key-value pairs storage
+     */
+    robots_io_std_field_storage(string);
+
+    robots_io_field_storage(double);
+
+    robots_io_field_storage(int);
+
+    robots_io_field_storage(bool);
+};
+
+#endif /* ROBOTSIO_PARAMETERS_H */
---
+++ src/RobotsIO/src/Utils/Parameters.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <RobotsIO/Utils/Parameters.h>
+
+using namespace RobotsIO::Utils;
+
+#define robots_io_field_getter_impl(type) \
+    type Parameters::get_##type(const std::string& name) const \
+    { \
+        return type##_data_.at(name); \
+    } \
+
+#define robots_io_std_field_getter_impl(type) \
+    std::type Parameters::get_##type(const std::string& name) const \
+    { \
+        return type##_data_.at(name); \
+    } \
+
+#define robots_io_field_setter_impl(type) \
+    void Parameters::set_##type(const std::string& name, const type& value) \
+    { \
+        type##_data_[name] = value; \
+    } \
+
+#define robots_io_std_field_setter_impl(type) \
+    void Parameters::set_##type(const std::string& name, const std::type& value) \
+    { \
+        type##_data_[name] = value; \
+    }
+
+
+robots_io_std_field_getter_impl(string);
+
+
+robots_io_field_getter_impl(double);
+
+
+robots_io_field_getter_impl(int);
+
+
+robots_io_field_getter_impl(bool);
+
+
+robots_io_std_field_setter_impl(string);
+
+
+robots_io_field_setter_impl(double);
+
+
+robots_io_field_setter_impl(int);
+
+
+robots_io_field_setter_impl(bool);
