40e1975ed56fa632f44d6b9428ebcee317774712
Nicola Piga <nicolapiga@gmail.com>
2019-12-20 01:22:03 +0100

    Implement class Camera::DatasetCamera

---
+++ src/RobotsIO/include/RobotsIO/Camera/DatasetCamera.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_DATASETCAMERA_H
+#define ROBOTSIO_DATASETCAMERA_H
+
+#include <RobotsIO/Camera/Camera.h>
+
+#include <string>
+
+namespace RobotsIO {
+    namespace Camera {
+        class DatasetCamera;
+    }
+}
+
+
+class RobotsIO::Camera::DatasetCamera : public RobotsIO::Camera::Camera
+{
+public:
+    DatasetCamera(const std::string& data_path, const std::string& data_prefix, const std::string& rgb_prefix, const std::string& depth_prefix, const std::string& data_format, const std::string& rgb_format, const std::string& depth_format, const std::size_t& heading_zeros, const std::size_t& index_offset, const std::size_t& width, const double& height, const double& fx, const double& cx, const double& fy, const double& cy);
+
+    virtual ~DatasetCamera();
+
+    /**
+     * RGB-D and pose.
+     */
+
+    virtual std::pair<bool, Eigen::MatrixXf> depth(const bool& blocking) override;
+
+    virtual std::pair<bool, Eigen::Transform<double, 3, Eigen::Affine>> pose(const bool& blocking) override;
+
+    virtual std::pair<bool, cv::Mat> rgb(const bool& blocking) override;
+
+private:
+    const std::string log_name_ = "DatasetCamera";
+};
+
+#endif /* ROBOTSIO_CAMERA_H */
---
+++ src/RobotsIO/src/Camera/DatasetCamera.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <RobotsIO/Camera/DatasetCamera.h>
+
+using namespace RobotsIO::Camera;
+
+
+DatasetCamera::DatasetCamera
+(
+    const std::string& data_path,
+    const std::string& data_prefix,
+    const std::string& rgb_prefix,
+    const std::string& depth_prefix,
+    const std::string& data_format,
+    const std::string& rgb_format,
+    const std::string& depth_format,
+    const std::size_t& heading_zeros,
+    const std::size_t& index_offset,
+    const std::size_t& width,
+    const double& height,
+    const double& fx,
+    const double& cx,
+    const double& fy,
+    const double& cy
+) :
+    Camera(data_path, width, height, fx, cx, fy, cy)
+{
+    /* Set dataset parameters. */
+    dataset_parameters_.data_prefix(data_prefix);
+    dataset_parameters_.rgb_prefix(rgb_prefix);
+    dataset_parameters_.depth_prefix(depth_prefix);
+    dataset_parameters_.data_format(data_format);
+    dataset_parameters_.rgb_format(rgb_format);
+    dataset_parameters_.depth_format(depth_format);
+    dataset_parameters_.heading_zeros(heading_zeros);
+    dataset_parameters_.index_offset(index_offset);
+
+    Camera::initialize();
+}
+
+
+DatasetCamera::~DatasetCamera()
+{ }
+
+
+std::pair<bool, Eigen::MatrixXf> DatasetCamera::depth(const bool& blocking)
+{
+    return depth_offline();
+}
+
+
+std::pair<bool, Eigen::Transform<double, 3, Eigen::Affine>> DatasetCamera::pose(const bool& blocking)
+{
+    return pose_offline();
+}
+
+
+std::pair<bool, cv::Mat> DatasetCamera::rgb(const bool& blocking)
+{
+    return rgb_offline();
+}
