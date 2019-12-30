46b437a9228b79dc1e3ba25026353d76a027c285
Nicola Piga <nicolapiga@gmail.com>
2019-12-30 01:18:42 +0100

    Implement abstract class ParametersFiller

---
+++ src/RobotsIO/include/RobotsIO/Utils/ParametersFiller.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_PARAMETERS_FILLER_H
+#define ROBOTSIO_PARAMETERS_FILLER_H
+
+#include <string>
+
+namespace RobotsIO {
+    namespace Utils {
+        class ParametersFiller;
+    }
+}
+
+
+class RobotsIO::Utils::ParametersFiller
+{
+public:
+    virtual const std::pair<bool, std::string> fill_string(const std::string& key) const = 0;
+
+    virtual const std::pair<bool, std::size_t> fill_size_t(const std::string& key) const = 0;
+
+    virtual const std::pair<bool, int> fill_int(const std::string& key) const = 0;
+
+    virtual const std::pair<bool, double> fill_double(const std::string& key) const = 0;
+
+    virtual const std::pair<bool, bool> fill_bool(const std::string& key) const = 0;
+};
+
+#endif /* ROBOTSIO_PARAMETERS_FILLER_H */
