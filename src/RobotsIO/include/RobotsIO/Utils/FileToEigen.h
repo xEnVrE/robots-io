7444d23ba3f277117c1aef6f0bc96f82a016b2e6
Nicola Piga <nicolapiga@gmail.com>
2020-03-05 11:30:19 +0100

    Implement method RobotsIO::Utils::file_to_eigen()

---
+++ src/RobotsIO/include/RobotsIO/Utils/FileToEigen.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_FILETOEIGEN_H
+#define ROBOTSIO_FILETOEIGEN_H
+
+#include <Eigen/Dense>
+
+namespace RobotsIO {
+    namespace Utils {
+        std::pair<bool, Eigen::MatrixXd> file_to_eigen(const std::string& file_path, const std::size_t& skip_rows, const std::size_t skip_cols, const std::size_t& expected_cols);
+    }
+}
+
+#endif /* ROBOTSIO_FILETOEIGEN_H */
---
+++ src/RobotsIO/src/Utils/FileToEigen.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <RobotsIO/Utils/FileToEigen.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace Eigen;
+
+
+std::pair<bool, Eigen::MatrixXd> RobotsIO::Utils::file_to_eigen(const std::string& file_path, const std::size_t& skip_rows, const std::size_t skip_cols, const std::size_t& expected_cols)
+{
+    MatrixXd data;
+
+    std::ifstream istrm(file_path);
+    if (!istrm.is_open())
+    {
+        std::cerr << "RobotsIO::Utils::file_to_eigen. Error: failed to open " << file_path << std::endl;
+
+        return std::make_pair(false, MatrixXd());
+    }
+
+    std::vector<std::string> istrm_strings;
+    std::string line;
+    {
+        std::size_t rows = -1;
+        while (std::getline(istrm, line))
+        {
+            rows++;
+
+            if (rows < skip_rows)
+                continue;
+
+            istrm_strings.push_back(line);
+        }
+    }
+
+    istrm.close();
+
+    data.resize(expected_cols, istrm_strings.size());
+    std::size_t found_lines = 0;
+    for (auto line : istrm_strings)
+    {
+        std::size_t cols = -1;
+        std::size_t found_fields = 0;
+        std::string number_str;
+        std::istringstream iss(line);
+
+        while (iss >> number_str)
+        {
+            cols++;
+
+            if (cols < skip_cols)
+                continue;
+
+            if (found_fields > expected_cols)
+            {
+                std::cerr << "RobotsIO::Utils::file_to_eigen. Error: malformed input file " << file_path << "." << std::endl
+                          << "Detailed error: found more columns than expected." << std::endl;
+
+                return std::make_pair(false, MatrixXd());
+            }
+
+            try
+            {
+                std::size_t index = (expected_cols * found_lines) + found_fields;
+                *(data.data() + index) = std::stod(number_str);
+            }
+            catch (std::invalid_argument)
+            {
+                std::cerr << "RobotsIO::Utils::file_to_eigen. Error: malformed input file " << file_path << "." << std::endl
+                          << "Detailed error: data cannot be interpreted as double "
+                          << "at line " + std::to_string(skip_cols + found_lines) << ", "
+                          << "token " + std::to_string(found_fields) << std::endl;
+
+                return std::make_pair(false, MatrixXd());
+            }
+
+            found_fields++;
+        }
+
+        if (found_fields != expected_cols)
+        {
+            std::cerr << "RobotsIO::Utils::read_data_from_file. Error: malformed input file " << file_path << std::endl
+                      << "Detailed error: found less columns than expected." << std::endl;
+
+            return std::make_pair(false, MatrixXd());
+        }
+        found_lines++;
+    }
+
+    return std::make_pair(true, data);
+}
