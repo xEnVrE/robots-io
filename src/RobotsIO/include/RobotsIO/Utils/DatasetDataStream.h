76e3f75a1ad202a14ab7353fed732092c2ca7e21
Nicola Piga <nicolapiga@gmail.com>
2020-03-21 18:22:11 +0100

    Implement class DatasetDataStream
    A DataStream abstraction for CSV file of floating point numbers with rx/tx times

---
+++ src/RobotsIO/include/RobotsIO/Utils/DatasetDataStream.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_DATASETDATASTREAM_H
+#define ROBOTSIO_DATASETDATASTREAM_H
+
+#include <Eigen/Dense>
+
+namespace RobotsIO {
+    namespace Utils {
+        class DatasetDataStream;
+    }
+}
+
+
+class RobotsIO::Utils::DatasetDataStream
+{
+public:
+    static const int NoTimeIndex = -1;
+
+    DatasetDataStream(const std::string& file_path, const std::size_t& skip_rows, const std::size_t& skip_cols, const std::size_t& expected_cols, const int rx_time_index = NoTimeIndex, const int tx_time_index = NoTimeIndex);
+
+    virtual ~DatasetDataStream();
+
+    double rx_time();
+
+    double tx_time();
+
+    Eigen::VectorXd data();
+
+    bool freeze();
+
+private:
+    Eigen::MatrixXd data_;
+
+    Eigen::VectorXd data_rx_time_;
+
+    Eigen::VectorXd data_tx_time_;
+
+    int rx_time_index_;
+
+    int tx_time_index_;
+
+    int head_ = -1;
+
+    const std::string log_name_ = "DatasetDataStream";
+};
+
+#endif /* ROBOTSIO_DATASETDATASTREAM_H */
---
+++ src/RobotsIO/src/Utils/DatasetDataStream.cpp
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <RobotsIO/Utils/DatasetDataStream.h>
+#include <RobotsIO/Utils/FileToEigen.h>
+
+using namespace Eigen;
+using namespace RobotsIO::Utils;
+
+
+DatasetDataStream::DatasetDataStream(const std::string& file_path, const std::size_t& skip_rows, const std::size_t& skip_cols, const std::size_t& expected_cols, const int rx_time_index, const int tx_time_index)
+{
+    bool valid_file;
+    MatrixXd data_all;
+    std::tie(valid_file, data_all) = file_to_eigen(file_path, skip_rows, skip_cols, expected_cols);
+
+    if (!valid_file)
+        throw(std::runtime_error(log_name_ + "::ctor. Error cannot read data from file " + file_path + "."));
+
+    if (rx_time_index >= data_all.cols())
+        throw(std::runtime_error(log_name_ + "::ctor. Specified rx time index " + std::to_string(rx_time_index) + " is out of range."));
+
+    if (tx_time_index >= data_all.cols())
+        throw(std::runtime_error(log_name_ + "::ctor. Specified tx time index " + std::to_string(tx_time_index) + " is out of range."));
+
+    std::size_t data_time_rows = 0;
+    if (rx_time_index != -1)
+    {
+        data_rx_time_ = data_all.row(rx_time_index);
+        data_time_rows++;
+    }
+
+    if (tx_time_index != -1)
+    {
+        data_tx_time_ = data_all.row(tx_time_index);
+        data_time_rows++;
+    }
+
+    data_.resize(data_all.rows() - data_time_rows, data_all.cols());
+    std::size_t j = 0;
+    for (std::size_t i = 0; i < data_all.rows(); i++)
+    {
+        if ((i == rx_time_index) || (i == tx_time_index))
+            continue;
+        data_.row(j) = data_all.row(i);
+
+        j++;
+    }
+}
+
+
+DatasetDataStream::~DatasetDataStream()
+{}
+
+
+double DatasetDataStream::rx_time()
+{
+    if (data_rx_time_.size() != 0)
+        return data_rx_time_(head_);
+
+    return 0.0;
+}
+
+
+double DatasetDataStream::tx_time()
+{
+    if (data_tx_time_.size() != 0)
+        return data_tx_time_(head_);
+
+    return 0.0;
+}
+
+
+VectorXd DatasetDataStream::data()
+{
+    return data_.col(head_);
+}
+
+
+bool DatasetDataStream::freeze()
+{
+    /* Step the head. */
+    head_++;
+
+    if (head_ >= data_.cols())
+        return false;
+
+    return true;
+}
