e054aa7707863328d79cd07de219ec42b9f18c4c
Nicola Piga <nicolapiga@gmail.com>
2020-04-02 21:15:01 +0200

    Implement class ImageFileProbe
    It acts as a probe receiving a cv::Mat and saving it to file

---
+++ src/RobotsIO/include/RobotsIO/Utils/ImageFileProbe.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_IMAGEFILEPROBE_H
+#define ROBOTSIO_IMAGEFILEPROBE_H
+
+#include <RobotsIO/Utils/Data.h>
+#include <RobotsIO/Utils/Probe.h>
+#include <RobotsIO/Utils/any.h>
+
+#include <string>
+
+#include <yarp/cv/Cv.h>
+
+namespace RobotsIO {
+    namespace Utils {
+        class ImageFileProbe;
+    }
+}
+
+
+class RobotsIO::Utils::ImageFileProbe : public RobotsIO::Utils::Probe
+{
+public:
+    ImageFileProbe(const std::string& output_path, const std::string& prefix, const std::string& output_format);
+
+    virtual ~ImageFileProbe();
+
+protected:
+    void on_new_data() override;
+
+private:
+    cv::Mat data_cv_;
+
+    std::string output_prefix_;
+
+    const std::string output_format_;
+
+    std::size_t frame_counter_ = 0;
+
+    const std::string log_name_ = "ImageFileProbe";
+};
+
+#endif /* ROBOTSIO_IMAGEFILEPROBE_H */
---
+++ src/RobotsIO/src/Utils/ImageFileProbe.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <RobotsIO/Utils/ImageFileProbe.h>
+
+#include <opencv2/opencv.hpp>
+
+using namespace RobotsIO::Utils;
+
+
+ImageFileProbe::ImageFileProbe(const std::string& output_path, const std::string& prefix, const std::string& output_format) :
+    output_prefix_(output_path),
+    output_format_(output_format)
+{
+    if (output_prefix_.back() != '/')
+        output_prefix_ += '/';
+
+    output_prefix_ += (prefix + "_");
+}
+
+
+ImageFileProbe::~ImageFileProbe()
+{}
+
+
+void ImageFileProbe::on_new_data()
+{
+    data_cv_ = RobotsIO::Utils::any_cast<cv::Mat>(get_data());
+
+    cv::imwrite(output_prefix_ + std::to_string(frame_counter_) + "." + output_format_, data_cv_);
+
+    frame_counter_++;
+}
