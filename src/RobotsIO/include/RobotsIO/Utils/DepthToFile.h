97c6cf07a62aa7f15bcd2f81e0a46b5a65734482
Nicola Piga <nicolapiga@gmail.com>
2020-05-07 19:35:32 +0200

    Implement RobotsIO::Utils::depth_to_file()

---
+++ src/RobotsIO/include/RobotsIO/Utils/DepthToFile.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_DEPTHTOFILE_H
+#define ROBOTSIO_DEPTHTOFILE_H
+
+#include <Eigen/Dense>
+
+#include <opencv2/opencv.hpp>
+
+namespace RobotsIO {
+    namespace Utils {
+        bool depth_to_file(const std::string& output_path, const cv::Mat& depth);
+
+        bool depth_to_file(const std::string& output_path, const Eigen::MatrixXf& depth);
+    }
+}
+
+#endif /* ROBOTSIO_DEPTHTOFILE_H */
---
+++ src/RobotsIO/src/Utils/DepthToFile.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <RobotsIO/Utils/DepthToFile.h>
+
+#include <opencv2/core/eigen.hpp>
+
+using namespace Eigen;
+
+
+bool RobotsIO::Utils::depth_to_file(const std::string& output_path, const cv::Mat& depth)
+{
+    const std::string log_name = "RobotsIO::Utils::depth_to_file";
+
+    std::FILE* out;
+
+    if ((out = std::fopen(output_path.c_str(), "wb")) == nullptr)
+    {
+        std::cout << log_name << "Error: cannot open output file " + output_path << std::endl;
+        return false;
+    }
+
+    /* Write image size. */
+    std::size_t dims[2];
+    dims[0] = depth.cols;
+    dims[1] = depth.rows;
+    if (std::fwrite(dims, sizeof(dims), 1, out) != 1)
+    {
+        std::cout << log_name << "Error: cannot write image size to " + output_path << std::endl;
+        return false;
+    }
+
+    if (std::fwrite(depth.data, sizeof(float), dims[0] * dims[1], out) != dims[0] * dims[1])
+    {
+        std::cout << log_name << "Error: cannot write image data to " + output_path << std::endl;
+        return false;
+    }
+
+    std::fclose(out);
+
+    return true;
+}
+
+bool RobotsIO::Utils::depth_to_file(const std::string& output_path, const MatrixXf& depth)
+{
+    cv::Mat depth_cv;
+    cv::eigen2cv(depth, depth_cv);
+    return RobotsIO::Utils::depth_to_file(output_path, depth_cv);
+}
