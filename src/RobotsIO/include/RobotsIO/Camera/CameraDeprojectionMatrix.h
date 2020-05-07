44e4ba816b1004c02fc827e855cd5533cf1880ec
Nicola Piga <nicolapiga@gmail.com>
2020-05-07 15:11:34 +0200

    Implement RobotsIO::Camera::CameraDeprojectionMatrix()

---
+++ src/RobotsIO/include/RobotsIO/Camera/CameraDeprojectionMatrix.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_CAMERADEPROJECTIONMATRIX_H
+#define ROBOTSIO_CAMERADEPROJECTIONMATRIX_H
+
+#include <Eigen/Dense>
+
+namespace RobotsIO {
+    namespace Camera {
+        Eigen::MatrixXd deprojection_matrix(const std::size_t& width, const std::size_t& height, const double& fx, const double& fy, const double& cx, const double& cy);
+    }
+}
+
+#endif
---
+++ src/RobotsIO/src/Camera/CameraDeprojectionMatrix.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <RobotsIO/Camera/CameraDeprojectionMatrix.h>
+
+using namespace Eigen;
+
+
+MatrixXd RobotsIO::Camera::deprojection_matrix(const std::size_t& width, const std::size_t& height, const double& fx, const double& fy, const double& cx, const double& cy)
+{
+    MatrixXd deprojection_matrix(3, width * height);
+
+    std::size_t i = 0;
+    for (std::size_t u = 0; u < width; u++)
+    {
+        for (std::size_t v = 0; v < height; v++)
+        {
+            deprojection_matrix(0, i) = (u - cx) / fx;
+            deprojection_matrix(1, i) = (v - cy) / fy;
+            deprojection_matrix(2, i) = 1.0;
+
+            i++;
+        }
+    }
+
+    return deprojection_matrix;
+}
