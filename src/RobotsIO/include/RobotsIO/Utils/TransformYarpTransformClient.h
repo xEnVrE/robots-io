d846ea54ec0008e07394fcf0386b4a97806623d3
Nicola Piga <nicolapiga@gmail.com>
2020-05-29 01:30:29 +0200

    Implement class RobotsIO::Utils::TransformYarpTransformClient

---
+++ src/RobotsIO/include/RobotsIO/Utils/TransformYarpTransformClient.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_TRANSFORMYARPVECTOR_H
+#define ROBOTSIO_TRANSFORMYARPVECTOR_H
+
+#include <RobotsIO/Utils/Transform.h>
+
+#include <Eigen/Dense>
+
+#include <string>
+
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/IFrameTransform.h>
+#include <yarp/os/Network.h>
+
+namespace RobotsIO {
+    namespace Utils {
+        class TransformYarpTransformClient;
+    }
+}
+
+
+class RobotsIO::Utils::TransformYarpTransformClient : public RobotsIO::Utils::Transform
+{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    TransformYarpTransformClient(const std::string& port_prefix, const std::string& source_name, const std::string& target_name);
+
+    virtual ~TransformYarpTransformClient();
+
+    Eigen::Transform<double, 3, Eigen::Affine> transform() override;
+
+    bool freeze(const bool blocking = false) override;
+
+private:
+    yarp::os::Network yarp_;
+
+    yarp::dev::PolyDriver drv_transform_client_;
+
+    yarp::dev::IFrameTransform* transform_client_;
+
+    Eigen::Transform<double, 3, Eigen::Affine> transform_;
+
+    const std::string source_name_;
+
+    const std::string target_name_;
+
+    const std::string log_name_ = "TransformYarpTransformClient";
+};
+
+#endif /* ROBOTSIO_TRANSFORMYARPVECTOR_H */
---
+++ src/RobotsIO/src/Utils/TransformYarpTransformClient.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <RobotsIO/Utils/TransformYarpTransformClient.h>
+
+#include <yarp/eigen/Eigen.h>
+#include <yarp/os/Property.h>
+
+using namespace Eigen;
+using namespace RobotsIO::Utils;
+using namespace yarp::eigen;
+
+
+TransformYarpTransformClient::TransformYarpTransformClient(const std::string& port_prefix, const std::string& source_name, const std::string& target_name) :
+    source_name_(source_name),
+    target_name_(target_name)
+{
+    /* FrameTransformClient initialization. */
+    yarp::os::Property tf_properties;
+    tf_properties.put("device", "transformClient");
+    tf_properties.put("local", "/" + port_prefix + "/transform_client");
+    tf_properties.put("remote", "/transformServer");
+
+    bool ok = drv_transform_client_.open(tf_properties);
+    ok &= (drv_transform_client_.view(transform_client_) && (transform_client_ != nullptr));
+    if (!ok)
+    {
+        throw(std::runtime_error(log_name_ + "::ctor. Error: cannot initialize the FrameTransformClient."));
+    }
+}
+
+
+TransformYarpTransformClient:: ~TransformYarpTransformClient()
+{}
+
+
+Eigen::Transform<double, 3, Affine> TransformYarpTransformClient::transform()
+{
+    return transform_;
+}
+
+
+bool TransformYarpTransformClient::freeze(const bool blocking)
+{
+    yarp::sig::Matrix transform(4, 4);
+    if (!transform_client_->getTransform(target_name_, source_name_, transform))
+        return false;
+
+    transform_.matrix() = toEigen(transform);
+
+    return true;
+}
