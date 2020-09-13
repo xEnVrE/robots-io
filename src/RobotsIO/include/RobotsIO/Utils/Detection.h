ef2a84e0c558c25b05e4d7e812b4a4cc5a29d88e
Nicola Piga <nicolapiga@gmail.com>
2020-09-13 14:15:20 +0200

    Add RobotsIO::Utils::Detection

---
+++ src/RobotsIO/include/RobotsIO/Utils/Detection.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ROBOTSIO_DETECTION_H
+#define ROBOTSIO_DETECTION_H
+
+#include <RobotsIO/Utils/DataStream.h>
+
+#include <Eigen/Dense>
+
+#include <opencv2/opencv.hpp>
+
+namespace RobotsIO {
+    namespace Utils {
+        class Detection;
+    }
+}
+
+
+class RobotsIO::Utils::Detection : public RobotsIO::Utils::DataStream
+{
+public:
+    virtual ~Detection();
+
+    virtual cv::Rect detection() = 0;
+};
+
+#endif /* ROBOTSIO_DETECTION_H */
---
+++ src/RobotsIO/src/Utils/Detection.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <RobotsIO/Utils/Detection.h>
+
+using namespace RobotsIO::Utils;
+
+
+Detection::~Detection()
+{}
