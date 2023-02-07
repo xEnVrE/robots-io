8c4c05baa70c1d1c712e366b8e9c25fece276ee8
Nicola Piga <nicolapiga@gmail.com>
2023-02-07 14:20:46 +0100

    GPL -> BSD-3

--- src/RobotsIO/include/RobotsIO/Utils/DepthToFile.h
+++ src/RobotsIO/include/RobotsIO/Utils/DepthToFile.h
@@ -2,7 +2,7 @@
  * Copyright (C) 2020 Istituto Italiano di Tecnologia (IIT)
  *
  * This software may be modified and distributed under the terms of the
- * GPL-2+ license. See the accompanying LICENSE file for details.
+ * BSD 3-Clause license. See the accompanying LICENSE file for details.
  */
 
 #ifndef ROBOTSIO_DEPTHTOFILE_H
